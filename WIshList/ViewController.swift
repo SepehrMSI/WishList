ca465d6ca6f8794e475a90d97691039bdb22905a
Sepehr <sepehr.masoudi@gmail.com>
2017-07-24 10:42:33 +0430

    Initial Commit

---
+++ WIshList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WIshList
+//
+//  Created by Sepehr's Cool Macbook on 7/24/17.
+//  Copyright © 2017 Sepehr's Cool Macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
