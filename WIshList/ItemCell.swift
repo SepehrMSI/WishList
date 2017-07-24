e7b40aec92280dbb6742f0cae31ce670e4c7cce3
Sepehr <sepehr.masoudi@gmail.com>
2017-07-24 21:57:33 +0430

    E67

---
+++ WIshList/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  WIshList
+//
+//  Created by Sepehr's Cool Macbook on 7/24/17.
+//  Copyright © 2017 Sepehr's Cool Macbook. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+}
--- WIshList/MianVC.swift
+++ WIshList/MianVC.swift
@@ -4,22 +4,45 @@
 //
 //  Created by Sepehr's Cool Macbook on 7/24/17.
 //  Copyright © 2017 Sepehr's Cool Macbook. All rights reserved.
-//  BlindSpot
+//
 
 import UIKit
 
-class MainVC: UIViewController {
-
+class MainVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
