bcc3c1da7bdbc28d7eecf36a2ac1551465ea5309
Sepehr <sepehr.masoudi@gmail.com>
2017-07-24 19:37:27 +0430

    UI and materila design completed (E66)

--- Item+CoreDataClass.swift
+++ Item+CoreDataClass.swift
@@ -11,5 +11,19 @@ import CoreData
 
 
 public class Item: NSManagedObject {
+    
+    public override func awakeFromInsert() {
+        
+        super.awakeFromInsert()
+        
+        self.created = NSDate()
+        
+        
+        
+    }
+    
+    
+    
+    
 
 }
