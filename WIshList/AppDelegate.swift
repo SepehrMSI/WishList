313eeb796204a2194c60fe536ef5885457559e0b
Sepehr <sepehr.masoudi@gmail.com>
2017-07-26 20:51:25 +0430

    Middle of core data ( Episode 68 Finished)

--- WIshList/AppDelegate.swift
+++ WIshList/AppDelegate.swift
@@ -89,5 +89,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
+
 }
+
+let ad = UIApplication.shared.delegate as! AppDelegate
+let context = ad.persistentContainer.viewContext
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
